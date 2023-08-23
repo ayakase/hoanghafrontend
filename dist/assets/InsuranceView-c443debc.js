41ce9d8ce6d936ee5a6d6d0b03c15fbdbc2b34f2
ayakase <wibu1892001@gmail.com>
2023-08-23 20:46:37 +0700

    a

---
+++ dist/assets/InsuranceView-c443debc.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c}from"./index-e598cca9.js";const n={};function t(r,s,a,_,p,i){return o(),c("div",null," Bảo hiểm ")}const l=e(n,[["render",t]]);export{l as default};
--- dist/assets/ProcedureView-fd3de995.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c}from"./index-bfbc37cc.js";const o={};function r(n,s,a,_,p,l){return t(),c("div",null," Quy trình đặt lịch ")}const d=e(o,[["render",r]]);export{d as default};
